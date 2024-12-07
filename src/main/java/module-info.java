3fa06db49da909c855a41ed53019b24f66abead9
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2024-12-07 23:43:18 +0600

    Start reprosetory

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module oop.keyacosmeticsltd.keya_cosmetics__ltd {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens oop.keyacosmeticsltd.keya_cosmetics__ltd to javafx.fxml;
+    exports oop.keyacosmeticsltd.keya_cosmetics__ltd;
+}
---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/HelloController.java
@@ -0,0 +1,14 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
