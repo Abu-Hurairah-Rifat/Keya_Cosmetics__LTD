d4228d0394e03d2a4b1a853c692e225ed1efe582
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2024-12-12 03:40:08 +0600

    HR Manager Edit employee

---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/EditEmployeeAccounts.java
@@ -0,0 +1,13 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EditEmployeeAccounts {
+
+    @FXML
+    void updateEmployeeDetailsButton(ActionEvent event) {
+
+    }
+
+}
