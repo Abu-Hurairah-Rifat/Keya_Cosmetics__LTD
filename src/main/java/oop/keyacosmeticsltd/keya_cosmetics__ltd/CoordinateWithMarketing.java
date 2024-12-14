b2ff603ae6b63f13a8bffa3f407c16bb7d905725
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2024-12-14 14:57:01 +0600

    rashad error solve

---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/CoordinateWithMarketing.java
@@ -0,0 +1,69 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CoordinateWithMarketing {
+
+    @FXML
+    private CheckBox TVcheckbox;
+
+    @FXML
+    private TableColumn<?, ?> approvalstatuscolumnbox;
+
+    @FXML
+    private TableColumn<?, ?> campaignNametablecolumn;
+
+    @FXML
+    private TextField campaignNametextfield;
+
+    @FXML
+    private TextField datetextfield;
+
+    @FXML
+    private Label headlinelabel;
+
+    @FXML
+    private TableColumn<?, ?> launchcolumnview;
+
+    @FXML
+    private TableColumn<?, ?> productcolumnview;
+
+    @FXML
+    private TextField productnametextfield;
+
+    @FXML
+    private TextField promotionbudgettextfield;
+
+    @FXML
+    private CheckBox socialmediacheckbox;
+
+    @FXML
+    private TableView<?> tableview;
+
+    @FXML
+    void Addactionbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backonactionbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearfieldsactionbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeactionbutton(ActionEvent event) {
+
+    }
+
+}
