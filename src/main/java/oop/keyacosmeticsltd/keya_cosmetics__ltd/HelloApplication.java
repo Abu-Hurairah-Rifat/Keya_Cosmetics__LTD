15d25d6e0f65179d07c5130a347470b0844e576a
Rashengi <rashadmuntasirifty@gmail.com>
2024-12-15 02:13:28 +0600

    life is joynot

--- src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/HelloApplication.java
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Product Development Manager/Product Develop Manager Dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sales Manager/Establish Sales Target.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Student Project");
         stage.setScene(scene);
