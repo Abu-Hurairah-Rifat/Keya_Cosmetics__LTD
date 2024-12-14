1b623ca80f304152b602d64dade8d7a9cabdb460
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2024-12-14 23:08:57 +0600

    Marketing Manager all

---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/TrackCampaignPerformance.java
@@ -0,0 +1,18 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class TrackCampaignPerformance {
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitButton(ActionEvent event) {
+
+    }
+
+}
