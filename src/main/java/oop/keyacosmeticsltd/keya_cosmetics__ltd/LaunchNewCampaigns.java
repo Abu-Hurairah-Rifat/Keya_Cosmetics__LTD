b420800d58884ad67e3ad91673d45be5abb7498f
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2024-12-14 21:42:08 +0600

    Marketing Manager

---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/LaunchNewCampaigns.java
@@ -0,0 +1,38 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class LaunchNewCampaigns {
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private TextField goals;
+
+    @FXML
+    private TextField mediaFilePath;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private TextField targetAudience;
+
+    @FXML
+    void LaunchButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/MarketingManagerDashboard.java
@@ -0,0 +1,53 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MarketingManagerDashboard {
+
+    @FXML
+    void analyzeSalesDataButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void collaborateWithSalesTeamButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void developPromotionalStrategiesButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lounchNewCampaignsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageAdvertisingBudgetButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageSocialMediaEngagementButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void researchMarketTrendsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackCampaignPerformanceButton(ActionEvent event) {
+
+    }
+
+}
