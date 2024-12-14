76544475150d168509bc3e6d53b0de7700b86d67
coconutbae <nj91291@gmail.com>
2024-12-14 17:25:25 +0600

    3 push $

---
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/DailySalesController$.java
@@ -0,0 +1,70 @@
+package oop.keyacosmeticsltd.keya_cosmetics__ltd;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class DailySalesController$ {
+
+@FXML
+private TextField nameField;
+
+@FXML
+private Label nameLabel;
+
+@FXML
+private TableColumn<?, ?> nameTable;
+
+@FXML
+private TableView<?> table;
+
+@FXML
+private Label tittleDailySalesView;
+
+@FXML
+private TextField totalSaleField;
+
+@FXML
+private Label totalSaleLabel;
+
+@FXML
+private TextField unitSoldField;
+
+@FXML
+private Label unitSoldLabel;
+
+@FXML
+private TableColumn<?, ?> unitSoldTable;
+
+@FXML
+private TextField valueField;
+
+@FXML
+private Label valueLabel;
+
+@FXML
+private TableColumn<?, ?> valueTable;
+
+@FXML
+private AnchorPane view;
+
+@FXML
+void backButtonAction(ActionEvent event) {
+
+}
+
+@FXML
+void clearButtonAction(ActionEvent event) {
+
+}
+
+@FXML
+void enterButtonAction(ActionEvent event) {
+
+}
+
+}
--- src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/HelloApplication.java
+++ src/main/java/oop/keyacosmeticsltd/keya_cosmetics__ltd/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Managing Product Testing.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Store Display View $.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Product Develop Manager");
+        stage.setTitle("Daily Sales View");
         stage.setScene(scene);
         stage.show();
     }
